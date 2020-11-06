e66ec309be09e24ecabe27f7c8acd82d7f10baf4
ArielMarihuen <Ar.marihuen@alumnos.duoc.cl>
2020-11-06 17:27:23 -0300

    Creacion de primera app

--- TuCelu/settings.py
+++ TuCelu/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'TuCelu.app.appcelu',
+    'TuCelu.app.AppceluConfig',
 ]
 
 MIDDLEWARE = [
