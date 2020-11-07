dff696f807dd5d9c54ad887ab742c9efe34c65f0
ArielMarihuen <Ar.marihuen@alumnos.duoc.cl>
2020-11-06 22:08:20 -0300

    integraciom de clases celu y marca

--- TuCelu/settings.py
+++ TuCelu/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'TuCelu.app.AppceluConfig',
+    'appcelu',
 ]
 
 MIDDLEWARE = [
--- appcelu/models.py
+++ appcelu/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class Celular(models.Model):
+    marca = models.ForeignKey('appcelu.marca', on_delete=models.CASCADE)
+    nombre = models.CharField(max_length=150)
+    descripcion = models.TextField()
+    Color = models.CharField(max_length=150)
+    precio = models.CharField(max_length=15)
+    fecha_publicacion = models.DateTimeField(default = timezone.now)
+
+    def publish(self):
+        self.fecha_publicacion = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.nombre
+
+class Marca(models.Model):
+    nombre = models.CharField(max_length=150)
+
+    def __str__(self):
+        return self.nombre
+
+
+
